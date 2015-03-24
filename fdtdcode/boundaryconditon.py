93d0e1d492d19b87cd3fcc0448e8bcc9a8c1edb5
yutong pang <wstcpyt@gmail.com>
2015-03-24 14:40:58 -0400

    add relative permittivity function

--- fdtdcode/boundaryconditon.py
+++ fdtdcode/boundaryconditon.py
@@ -9,15 +9,15 @@ class TFSFboundarycondition():
 
     def get_incidence_source_correction(self, time_node_index, node_correction):
         if node_correction == 0:
-            correction = self.__get_magnetic_source_correction(time_node_index)
+            correction = self._get_magnetic_source_correction(time_node_index)
         else:
-            correction = self.__get_electric_source_correction(time_node_index)
+            correction = self._get_electric_source_correction(time_node_index)
         return correction
 
     @staticmethod
-    def __get_magnetic_source_correction(time_node_index):
+    def _get_magnetic_source_correction(time_node_index):
         return exp(-(time_node_index - 30.0)**2/100.0)/Meshnodefield.updatecoefficient
 
     @staticmethod
-    def __get_electric_source_correction(time_node_index):
+    def _get_electric_source_correction(time_node_index):
         return exp(-(time_node_index - 30.0 + 1)**2/100.0)
--- fdtdtest/teststructure.py
+++ fdtdtest/teststructure.py
@@ -1,10 +1,30 @@
 __author__ = 'yutongpang'
 import unittest
+import numpy as np
+from unittest.mock import patch
 from fdtdcode.structure import Permittivity
 
 
 class PermittivityTEST(unittest.TestCase):
-    def test_init_structure_permittivity(self):
-        permittivity = Permittivity(100)
-        size = len(permittivity.relative_permittivity)
-        self.assertEquals(size, 100)
+    def setUp(self):
+        self.permittivity = Permittivity(100)
+
+    @patch.object(Permittivity, '_set_relative_permittivity')
+    def test_init_structure_permittivity(self, mock_set_relative_permittivity):
+        self.permittivity = Permittivity(100)
+        mock_set_relative_permittivity.return_value = np.ones(self.permittivity.mesh_size)
+        mock_set_relative_permittivity.assert_called_once_with()
+
+    @patch.object(Permittivity, '_get_relative_permittivity_in_single_node')
+    def test_set_relative_permittivity(self, mock_get_relative_permittivity_in_single_node):
+        returnvalue = self.permittivity._set_relative_permittivity()
+        length = len(returnvalue)
+        self.assertEquals(length, self.permittivity.mesh_size)
+        callcount = mock_get_relative_permittivity_in_single_node.call_count
+        self.assertEquals(callcount, self.permittivity.mesh_size)
+
+    def test_get_relative_permittivity_in_single_node(self):
+        returnvalue_node_less_100 = self.permittivity._get_relative_permittivity_in_single_node(50)
+        self.assertEquals(returnvalue_node_less_100, 1)
+        returnvalue_node_equal_or_more_100 = self.permittivity._get_relative_permittivity_in_single_node(150)
+        self.assertEquals(returnvalue_node_equal_or_more_100, 9)
